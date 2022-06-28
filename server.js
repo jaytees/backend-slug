505b1a678db70870fc2c5e7073f1393c3721bb93
Jordan Trickett <jordan1trickett@gmail.com>
2022-06-28 15:03:27 +0100

    cors update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 //creates the express server on a specified port
 const app = express();
@@ -9,43 +9,40 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.options('*', cors());
 //lets us pass json
 app.use(express.json());
 
 //database uri - uniform resource identifier
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(
-  uri,
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-  err => {
-    // console.log('got error', err);
-  }
-);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
+  // console.log('got error', err);
+});
 
 //establishes connection to database
 const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established");
+connection.once('open', () => {
+  console.log('MongoDB database connection established');
 });
 
 //require the route files
-const outletRouter = require("./routes/outlet.route");
-const usersRouter = require("./routes/users.route");
-const nytimesRouter = require("./routes/nytimes.route");
-const hsRouter = require("./routes/hs.route");
-const slamcityRouter = require("./routes/slamcity.route");
-const pandpRouter = require("./routes/pandp.route");
+const outletRouter = require('./routes/outlet.route');
+const usersRouter = require('./routes/users.route');
+const nytimesRouter = require('./routes/nytimes.route');
+const hsRouter = require('./routes/hs.route');
+const slamcityRouter = require('./routes/slamcity.route');
+const pandpRouter = require('./routes/pandp.route');
 
 //now when a user goes to url(endpoint below)
 //it will use the relevant file specified above
-app.use("/outlet", outletRouter);
-app.use("/user", usersRouter);
+app.use('/outlet', outletRouter);
+app.use('/user', usersRouter);
 
-app.use("/nytimes", nytimesRouter);
-app.use("/hs", hsRouter);
-app.use("/slamcity", slamcityRouter);
-app.use("/pandp", pandpRouter);
+app.use('/nytimes', nytimesRouter);
+app.use('/hs', hsRouter);
+app.use('/slamcity', slamcityRouter);
+app.use('/pandp', pandpRouter);
 
 //starts the server listening on a certain port
 app.listen(port, () => {
